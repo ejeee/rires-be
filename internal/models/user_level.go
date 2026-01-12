25412b33cb998283ad80e2c78369511ac02614ad
ejeee <ejeitschool@gmail.com>
2026-01-12 15:20:15 +0700

    feat: deleted id_conference at user level models

--- internal/models/user_level.go
+++ internal/models/user_level.go
@@ -6,7 +6,6 @@ import "time"
 type UserLevel struct {
 	ID           int        `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
 	NamaLevel    string     `gorm:"column:nama_level;type:varchar(100)" json:"nama_level"`
-	IDConference int        `gorm:"column:id_conference;type:int;default:0" json:"id_conference"` // Add this field
 	Status       int        `gorm:"column:status;type:int(1);default:1" json:"status"`             // 1=active, 2=inactive
 	Hapus        int        `gorm:"column:hapus;type:int(1);default:0" json:"-"`                   // 0=exists, 1=deleted (hidden from JSON)
 	TglInsert    *time.Time `gorm:"column:tgl_insert;type:datetime" json:"tgl_insert"`
